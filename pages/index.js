6539b1ac22e1b9f1c5c0ba8480541333495adc7c
pcmike1 <michaels@physicianschoice.com>
2025-02-15 12:21:21 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
+// File: pages/index.js
+import React from 'react';
+import PromotionForm from '../components/PromotionForm';
 
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <PromotionForm />
+    </div>
+  );
+}
